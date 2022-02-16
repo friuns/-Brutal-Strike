eebf8a014c304fe8a38f74ba82665e93b7358477
friuns2 <dorumonstr@gmail.com>
2022-02-16 19:00:04 +0200

    d

--- Brutal Strike Update.bat
+++ Brutal Strike Update.bat
@@ -12,14 +12,7 @@ if not exist ./git/ (
 
 if not exist %SystemRoot%\System32\curl.exe (
 if not exist curl.exe (
-
-
-for %%i in (powershell.exe) do if "%%~$path:i"=="" (
-%SystemRoot%\system32\certutil.exe -urlcache -split -f https://github.com/git-for-windows/git/releases/download/v2.30.1.windows.1/PortableGit-2.30.1-32-bit.7z.exe gitSetup.exe
-) else (
 powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.30.1.windows.1/PortableGit-2.30.1-32-bit.7z.exe','gitSetup.exe')
-)
-
 )
 curl -L https://github.com/git-for-windows/git/releases/download/v2.30.1.windows.1/PortableGit-2.30.1-32-bit.7z.exe --output gitSetup.exe
 )
